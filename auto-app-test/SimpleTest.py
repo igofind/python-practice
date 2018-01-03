d90e62af54aa6eabb05c5b96460e657286b11417
sun <664006332@qq.com>
2018-01-03 13:02:28 +0800

    selenium:示例中添加自动等待

--- auto-app-test/SimpleTest.py
+++ auto-app-test/SimpleTest.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
 def main():
+    url = "https://free-ss.site/ss.json"
     chrome = webdriver.Chrome()
-    chrome.get("https://free-ss.site")
-    # 页面中作了定时，只能用强行等待
+    chrome.get(url)
     # 强行等待
-    time.sleep(6)
+    # time.sleep(6)
     # 隐性等待，最多等待10秒
     # chrome.implicitly_wait(10)
-    page = chrome.page_source
-    print(page)
-    chrome.quit()
+
+    try:
+        # 页面跳转后 title为空
+        WebDriverWait(chrome, 10).until(EC.title_is(""))
+        page = chrome.page_source
+        print(page)
+    finally:
+        chrome.quit()
 
 
 if '__main__' == __name__:
